c7232c86ecb74f47a5907a0086507a1ce4ce82f4
Hans Kristian Reynaldi <z5348378@ad.unsw.edu.au>
2023-12-28 07:31:00 +1100

    testing api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import User from "./models/user.model";
 
 dotenv.config();
 
@@ -11,10 +12,6 @@ const uri = process.env.MONGODB_URI;
 if (!uri) {
   throw new Error("MONGODB_URI is not defined in the environment variables");
 }
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
 
 const connect = async () => {
   try {
@@ -28,6 +25,27 @@ const connect = async () => {
 connect();
 
 app.use(express.json());
+
+app.get("/", async (req: Request, res: Response) => {
+  res.send("hello");
+});
+
+app.post("/user/create", async (req: Request, res: Response) => {
+  const detail = {
+    email: req.body.email,
+    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    photo: req.body.photo,
+  };
+  try {
+    const newUser = await User.create(detail);
+    res.status(200).json(newUser);
+  } catch (e) {
+    console.error(e);
+    res.status(400);
+  }
+});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
